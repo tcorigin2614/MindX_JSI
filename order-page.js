b75c7272e5b47b32ac80ce0801399419006874a3
tcorigin2614 <124500062+tcorigin2614@users.noreply.github.com>
2023-02-19 19:12:00 +0700

    5 version

--- order-page.js
+++ order-page.js
@@ -19,12 +19,25 @@ const checkoutHandler = () => {
     }
 };
 
-
+const calcTotalPrice = (arrItemPicked) => {
+    return arrItemPicked.reduce((acc, item) => {
+        var priceNumber = parseInt((item.saleprice.split(',')[0].toString() +  item.saleprice.split(',')[1].toString() + '000'))
+        return acc + priceNumber
+    }, 0)
+}
+const removeProduct = (id) => {
+    const myCart = JSON.parse(localStorage.getItem("hung_cart"))
+    const copyMyCart = myCart.filter(item => item.id !== id)
+    localStorage.setItem('hung_cart', JSON.stringify((copyMyCart)))
+    renderproductPicked(copyMyCart, "product-form")
+    const totalPrice = calcTotalPrice(copyMyCart)
+    document.getElementById('total-price').innerHTML = totalPrice.toLocaleString('it-IT', {style : 'currency', currency : 'VND'});
+}
 const renderproductPicked = (products, id) => {
     if( products && products.length > 0 ){
         const htmlString = products.reduce((acc, item)=> acc + `
-        <li class="flex flex-col py-6 sm:flex-row sm:justify-between product-cart">
-            <div class="flex w-full space-x-2 sm:space-x-4 border-product">
+        <li class="flex flex-col py-6 sm:flex-row sm:justify-between product-cart border-none">
+            <div class="flex w-full space-x-2 sm:space-x-4 border-product items-start">
                 <img class="flex-shrink-0 object-cover w-21 h-21 dark:border-transparent rounded outline-none sm:w-32 sm:h-32 dark:bg-gray-500" 
                 src= ${item.img}
                 <div class="flex flex-col justify-between w-full pb-4">
@@ -38,15 +51,8 @@ const renderproductPicked = (products, id) => {
                         </div>
                     </div>
                     <div class="flex text-sm divide-x">
-                        <button type="button" class="flex items-center px-2 py-1 pl-0 space-x-1 remove-btn">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-4 h-4 fill">
-                                <path d="M96,472a23.82,23.82,0,0,0,23.579,24H392.421A23.82,23.82,0,0,0,416,472V152H96Zm32-288H384V464H128Z"></path>
-                                <rect width="32" height="200" x="168" y="216"></rect>
-                                <rect width="32" height="200" x="240" y="216"></rect>
-                                <rect width="32" height="200" x="312" y="216"></rect>
-                                <path d="M328,88V40c0-13.458-9.488-24-21.6-24H205.6C193.488,16,184,26.542,184,40V88H64v32H448V88ZM216,48h80V88H216Z"></path>
-                            </svg>
-                            <span class="your-cart" style="color: white";>Remove</span>
+                        <button type="button" class="flex px-5 py-4  space-x-1 bg-red-600 rounded-lg" onclick="removeProduct(${item.id})">
+                            <span class=" your-cart" style="color: white";>Remove</span>
                         </button>
                     </div>
                 </div>
@@ -61,9 +67,24 @@ const renderproductPicked = (products, id) => {
     document.getElementById(id).innerHTML = htmlString
    }
 }
+
 const myCart = JSON.parse(localStorage.getItem("hung_cart"))
   renderproductPicked(myCart, "product-form")
- 
 
+var totalPrice = calcTotalPrice(myCart)
+document.getElementById('total-price').innerHTML = totalPrice.toLocaleString('it-IT', {style : 'currency', currency : 'VND'});
 
- 
+const renderEmptyCart = () => {
+
+}
+const purchase = () => {
+    Swal.fire({
+        title: 'Thanh toán thành công!',
+        icon: 'success',
+    })
+    localStorage.clear()
+    renderproductPicked([], 'product-form')
+    document.getElementById('total-price').innerHTML = `
+     0 đ
+    `
+}
